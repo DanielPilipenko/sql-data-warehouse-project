75e0095ac0fb091d9ed2833e94c86e18e20d2767
Daniel P. <daniel@test.de>
2026-06-06 18:35:31 +0200

    feat: add silver layer, load procedures and restructure docs

--- scripts/bronze/01_ddl_bronze_blueprint.sql
+++ scripts/bronze/01_ddl_bronze_blueprint.sql
@@ -56,28 +56,24 @@ IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL DROP TABLE bronze.crm_
 CREATE TABLE bronze.crm_sales_details (
     [sls_ord_num] NVARCHAR(MAX), [sls_prd_key] NVARCHAR(MAX), [sls_cust_id] NVARCHAR(MAX),
     [sls_order_dt] NVARCHAR(MAX), [sls_ship_dt] NVARCHAR(MAX), [sls_due_dt] NVARCHAR(MAX),
-    [sls_sales] NVARCHAR(MAX), [sls_quantity] NVARCHAR(MAX), [sls_price] NVARCHAR(MAX),
-    [extraction_date] DATETIME DEFAULT GETDATE()
+    [sls_sales] NVARCHAR(MAX), [sls_quantity] NVARCHAR(MAX), [sls_price] NVARCHAR(MAX)
 );
 GO
 
 IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL DROP TABLE bronze.erp_cust_az12;
 CREATE TABLE bronze.erp_cust_az12 (
-    [CID] NVARCHAR(MAX), [BDATE] NVARCHAR(MAX), [GEN] NVARCHAR(MAX),
-    [extraction_date] DATETIME DEFAULT GETDATE()
+    [CID] NVARCHAR(MAX), [BDATE] NVARCHAR(MAX), [GEN] NVARCHAR(MAX)
 );
 GO
 
 IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL DROP TABLE bronze.erp_loc_a101;
 CREATE TABLE bronze.erp_loc_a101 (
-    [CID] NVARCHAR(MAX), [CNTRY] NVARCHAR(MAX),
-    [extraction_date] DATETIME DEFAULT GETDATE()
+    [CID] NVARCHAR(MAX), [CNTRY] NVARCHAR(MAX)
 );
 GO
 
 IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL DROP TABLE bronze.erp_px_cat_g1v2;
 CREATE TABLE bronze.erp_px_cat_g1v2 (
-    [ID] NVARCHAR(MAX), [CAT] NVARCHAR(MAX), [SUBCAT] NVARCHAR(MAX), [MAINTENANCE] NVARCHAR(MAX),
-    [extraction_date] DATETIME DEFAULT GETDATE()
+    [ID] NVARCHAR(MAX), [CAT] NVARCHAR(MAX), [SUBCAT] NVARCHAR(MAX), [MAINTENANCE] NVARCHAR(MAX)
 );
 GO
